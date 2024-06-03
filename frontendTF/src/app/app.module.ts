d48f68e1f412e76722a78ca32c5ca1e9ac833bac
RicardoPando <85509984+RicardoPando@users.noreply.github.com>
2024-06-03 15:32:44 -0500

    creacion proyecto-falta crear ng g s services/----
    1-nuevo proyecto
    ng new nuevo-front --standalone=false
    
    cd nuevo-front
     
     
    2-material
    ng add @angular/material
     
    >ng g m modules/material

---
+++ frontendTF/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MaterialModule } from './modules/material/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

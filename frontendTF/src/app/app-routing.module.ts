8ca29a69f568473009929e9d4bdfa85d0d1f0aa5
RicardoPando <85509984+RicardoPando@users.noreply.github.com>
2024-06-15 16:48:57 -0500

    perfil alumno v0.2

--- frontendTF/src/app/app-routing.module.ts
+++ frontendTF/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { PerfilAlumnoComponent } from './components/alumno/perfil-alumno/perfil-alumno.component';
 
 
 const routes: Routes = [
   {path:"",component:HomeComponent},
   {path:"header",component:HeaderComponent},
-  {path:"footer",component:FooterComponent}
+  {path:"footer",component:FooterComponent},
+  {path:"perfil",component:PerfilAlumnoComponent}  
 ];
 
 @NgModule({

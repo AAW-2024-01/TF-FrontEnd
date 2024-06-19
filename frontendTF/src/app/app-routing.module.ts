5cde5cc611d1f8f38e849f9e0a8b8e5cdd59c251
RicardoPando <85509984+RicardoPando@users.noreply.github.com>
2024-06-19 07:16:03 -0500

    FRONTmockupsListos

--- frontendTF/src/app/app-routing.module.ts
+++ frontendTF/src/app/app-routing.module.ts
@@ -5,14 +5,18 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { PerfilAlumnoComponent } from './components/alumno/perfil-alumno/perfil-alumno.component';
 import { DetailAlumnoComponent } from './components/alumno/detail-alumno/detail-alumno.component'; 
+import { DetailAsesorComponent } from './components/asesor/detail-asesor/detail-asesor.component';
+import { PerfilAsesorComponent } from './components/asesor/perfil-asesor/perfil-asesor.component';
 
 
 const routes: Routes = [
   {path:"",component:HomeComponent},
   {path:"header",component:HeaderComponent},
   {path:"footer",component:FooterComponent},
-  {path:"perfil",component:PerfilAlumnoComponent} , 
-  {path:"perfiledit",component:DetailAlumnoComponent}  
+  {path:"perfilalumno",component:PerfilAlumnoComponent} , 
+  {path:"perfilasesor",component:PerfilAsesorComponent} , 
+  {path:"perfileditalumno",component:DetailAlumnoComponent},
+  {path:"perfileditasesor",component:DetailAsesorComponent}  
 ];
 
 @NgModule({
